e6c261fc778356ceced51b8f05c9b497b97ba313
unknown <gabriel.fritz@sfr.fr>
2019-02-25 18:00:54 +0100

    update to work experience and eductation, as well as severl little changes

---
+++ scripts/language.js
@@ -0,0 +1,10 @@
+let lang = window.navigator.language;
+window.addEventListener('load', redirect);
+
+function redirect(){
+  if (lang === "fr-FR" | lang === "fr-CH" | lang === "fr-CA" | lang === "fr-LU") {
+    window.location.href = "./fr/index.html";
+  } else {
+    window.location.href = "/en/index.html";
+  }
+}
