76d700577b7dec5ab1b4ab20f7cdf9ad3d33ee13
Spectralya <spectralr@outlook.fr>
2021-11-20 06:41:17 +0100

    updated

--- scripts/language.js
+++ scripts/language.js
@@ -5,6 +5,6 @@ function redirect(){
   if (lang === "fr-FR" | lang === "fr-CH" | lang === "fr-CA" | lang === "fr-LU") {
     window.location.href = "./fr/index.html";
   } else {
-    window.location.href = "/en/index.html";
+    window.location.href = "./en/index.html";
   }
 }
